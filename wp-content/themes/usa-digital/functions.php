0edfb007041cf13219610f77838299760b861cb9
ThroughEternity <eternity1666@gmail.com>
2022-05-22 01:18:47 +0400

    add class nav menu

--- wp-content/themes/usa-digital/functions.php
+++ wp-content/themes/usa-digital/functions.php
@@ -79,20 +79,54 @@ function usa_digital_menus() {
 // хук событие
 add_action( 'init', 'usa_digital_menus' );
 
-//добавление класса nav-item ко всем пунктам меню
+class bootstrap_4_walker_nav_menu extends Walker_Nav_menu {
+    
+	function start_lvl( &$output, $depth ){ // ul
+			$indent = str_repeat("\t",$depth); // indents the outputted HTML
+			$submenu = ($depth > 0) ? ' sub-menu' : '';
+			$output .= "\n$indent<ul class=\"dropdown-menu$submenu depth_$depth\">\n";
+	}
 
-add_filter( 'nav_menu_css_class', 'custom_nav_menu_css_class', 10, 1 );
-// получаем список всех классов пунктов меню 
-function custom_nav_menu_css_class ($classes) {
-	//добавляем к списку классов свой nav-item
-$classes[] = 'nav-item';
-//возвращаем список классов уже с нашим классом
-return $classes;
-}
-
-add_filter('nav_menu_link_attributes', 'custom_nav_menu_link_attributes');
-
-function custom_nav_menu_link_attributes ($atts) {
-$atts ['class'] = 'nav-link';
-return $atts;
+function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ){ // li a span
+			
+	$indent = ( $depth ) ? str_repeat("\t",$depth) : '';
+	
+	$li_attributes = '';
+			$class_names = $value = '';
+	
+			$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+			
+			$classes[] = ($args->walker->has_children) ? 'dropdown' : '';
+			$classes[] = ($item->current || $item->current_item_anchestor) ? 'active' : '';
+			$classes[] = 'nav-item';
+			$classes[] = 'nav-item-' . $item->ID;
+			if( $depth && $args->walker->has_children ){
+					$classes[] = 'dropdown-menu';
+			}
+			
+			$class_names =  join(' ', apply_filters('nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+			$class_names = ' class="' . esc_attr($class_names) . '"';
+			
+			$id = apply_filters('nav_menu_item_id', 'menu-item-'.$item->ID, $item, $args);
+			$id = strlen( $id ) ? ' id="' . esc_attr( $id ) . '"' : '';
+			
+			$output .= $indent . '<li ' . $id . $value . $class_names . $li_attributes . '>';
+			
+			$attributes = ! empty( $item->attr_title ) ? ' title="' . esc_attr($item->attr_title) . '"' : '';
+			$attributes .= ! empty( $item->target ) ? ' target="' . esc_attr($item->target) . '"' : '';
+			$attributes .= ! empty( $item->xfn ) ? ' rel="' . esc_attr($item->xfn) . '"' : '';
+			$attributes .= ! empty( $item->url ) ? ' href="' . esc_attr($item->url) . '"' : '';
+			
+			$attributes .= ( $args->walker->has_children ) ? ' class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"' : ' class="nav-link"';
+			
+			$item_output = $args->before;
+			$item_output .= ( $depth > 0 ) ? '<a class="dropdown-item"' . $attributes . '>' : '<a' . $attributes . '>';
+			$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+			$item_output .= '</a>';
+			$item_output .= $args->after;
+			
+			$output .= apply_filters ( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+	
+	}
+	
 }
--- wp-content/themes/usa-digital/header.php
+++ wp-content/themes/usa-digital/header.php
@@ -37,47 +37,19 @@
         </button>
 
         <div class="collapse navbar-collapse justify-content-end" id="mainNav">
-          <?php wp_nav_menu( [
+          <?php 
+          wp_nav_menu( [
 	'theme_location'  => 'header',
-	'container'       => 'false',
+	'container'       => false,
 	'menu_class'      => 'navbar-nav',
-	'menu_id'         => 'false',
+	'menu_id'         => false,
 	'echo'            => true,
 	'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-] ); ?>
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="/"> Главная </a>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarWelcome"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-                >О нас</a
-              >
-              <div class="dropdown-menu" aria-labelledby="navbarWelcome">
-                <a class="dropdown-item" href="about.html"> О компании </a>
-                <a class="dropdown-item" href="about.html"> Об услугах </a>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link smoth-scroll" href="service.html">Услуги</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link smoth-scroll" href="pricing.html">Цены</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link smoth-scroll" href="blog.html">Журнал</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link smoth-scroll" href="contact.html">Контакты</a>
-            </li>
-          </ul>
+	'depth'           => 2,
+	'walker'          => new bootstrap_4_walker_nav_menu(),
+] ); 
+?>
+          
         </div>
       </div>
     </nav>
